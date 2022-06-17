fda6c8766208a312eada23cd4a606ed03254e781
Russell Oyama <russelloyama@gmail.com>
2022-06-17 08:05:07 -1000

    moving player ship

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,18 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
+    [SerializeField] private float forceMagnitude;
+    [SerializeField] private float maxVelocity;
+
+    private Rigidbody rb;
     private Camera mainCamera;
+
+    private Vector3 movementDirection;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         mainCamera = Camera.main;
     }
 
@@ -19,13 +26,26 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
-            Vector2 touchPosition =  Touchscreen.current.primaryTouch.position.ReadValue();
-
-            Debug.Log(touchPosition);
+            Vector2 touchPosition =  Touchscreen.current.primaryTouch.position.ReadValue();            
 
             Vector3 worldPosition = mainCamera.ScreenToViewportPoint(touchPosition);
 
-            Debug.Log(worldPosition);
+            movementDirection = transform.position - worldPosition;
+            movementDirection.z = 0f;
+            movementDirection.Normalize();
         }
+        else
+        {
+            movementDirection = Vector3.zero;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(movementDirection == Vector3.zero) { return; }
+
+        rb.AddForce(movementDirection * forceMagnitude * Time.deltaTime, ForceMode.Force);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
     }
 }
